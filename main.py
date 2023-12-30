96660bb8b591ffce721e4c56414e690ed0b985f0
pavelsananko <144049324+pavelsananko@users.noreply.github.com>
2023-12-30 10:41:31 +0000

    added support for multiple pages; fixed an issue with free games; added filtering by rating; cleaned up some code

--- main.py
+++ main.py
@@ -1,75 +1,82 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://store.steampowered.com/search'
+URL = 'https://store.steampowered.com/search/?start={0}&count=50&maxprice={1}&category1=998&untags={2}&unvrsupport=401&os=win&supportedlang=english&ignore_preferences=1'
+PAGES = 2
 
-MAX_PAGES = 0 # TODO: use to stop the page loop; if 0, go until total_games
+MIN_RATING = 80
 MAX_PRICE = 30
-EXCLUDE = ['3799', '4085', '9130', '9551']
+BLACKLIST = ['3799', '4085', '9130', '9551']
 
 games = []
 
+# TODO: if PAGES = 0, go until total_games
 
+for page in range(PAGES):
+    url = URL.format(page * 50, MAX_PRICE, "%2C".join(BLACKLIST))
 
-# TODO: do the following for multiple pages
+    # send request and validate response
 
-page = 0
-url = f'{URL}/?start={page * 50}&count=50&maxprice={MAX_PRICE}&category1=998&sort_by=Reviews_DESC&ignore_preferences=1'
+    response = requests.get(url)
 
-# add excluded tags if there are any
+    if response.status_code != 200:
+        print(f'Error getting page {page} ({response.status_code})')
 
-if len(EXCLUDE) > 0:
-    url += f'&untags={"%2C".join(EXCLUDE)}'
+        continue
 
-# send request and validate response
+    # parse data from response
 
-response = requests.get(url)
+    content = BeautifulSoup(response.content, 'html.parser')
 
-if response.status_code != 200:
-    print(f'Error {response.status_code}')
-    exit() # TODO: replace with continue when in a loop
+    # TODO: maybe use this?
+    # total_games = int(content.find(class_='search_pagination_left').text.strip().split(' ')[-1])
 
-# parse data from response
+    rows = content.find_all(class_='search_result_row')
 
-content = BeautifulSoup(response.content, 'html.parser')
+    for row in rows:
+        link = row.get('href').split('?')[0]
+        name = row.find(class_='title').text
 
-# TODO: maybe use this?
-# total_games = int(content.find(class_='search_pagination_left').text.strip().split(' ')[-1])
+        # get reviews
 
-rows = content.find_all(class_='search_result_row')
+        elem = row.find(class_='search_review_summary').get('data-tooltip-html').split('<br>')[1]
+        review_percent = int(elem.split(' ')[0][:-1])
+        review_count = int(elem.split(' ')[3].replace(',', ''))
 
-for row in rows:
-    name = row.find(class_='title').text
-    link = row.get('href').split('?')[0]
+        # get original price
 
-    elem = row.find(class_='search_review_summary').get('data-tooltip-html').split('<br>')[1]
-    review_percent = int(elem.split(' ')[0][:-1])
-    review_count = int(elem.split(' ')[3].replace(',', ''))
+        elem = row.find(class_='discount_original_price')
+        if elem:
+            price_original = float(elem.text[:-1].replace(',', '.'))
+        else:
+            price_original = 0.0
 
-    elem = row.find(class_='discount_original_price')
-    if elem:
-        price_original = float(elem.text[:-1].replace(',', '.'))
-    else:
-        price_original = 0.0
+        # get final price
 
-    elem = row.find(class_='discount_final_price')
-    price = float(elem.text[:-1].replace(',', '.'))
+        elem = row.find(class_='discount_final_price')
+        if elem and elem.text != 'Free':
+            price = float(elem.text[:-1].replace(',', '.'))
+        else:
+            price = 0.0
 
-    if price_original != 0.0:
-        discount = round((1 - price / price_original) * 100)
-    else:
-        discount = 0
+        # calculate discount
 
-    # TODO: if possible, include tags/genre/type
-    # TODO: if possible, include length from HowLongToBeat
+        if price_original != 0.0:
+            discount = round((1 - price / price_original) * 100)
+        else:
+            discount = 0
 
-    games.append([name, link, review_percent, review_count, price, discount])
+        # TODO: if possible, include tags/genre/type
 
+        # filter and add game to list
 
+        if review_percent >= MIN_RATING:
+            games.append([link, name, review_percent, review_count, price, discount])
 
-# TODO: filter results
 # TODO: sort results
 # TODO: output into a file
 
 for game in games:
-    print(game[0])
+    print(game[1:])
+
+print('Total games: ', len(games))
