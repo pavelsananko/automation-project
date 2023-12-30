d682f8f18e642b817a164eb36ead6cf38d723c3f
pavelsananko <144049324+pavelsananko@users.noreply.github.com>
2023-12-30 15:53:51 +0000

    hard-coded excluded tags; added a minimum review count; added a request timeout; handled an exception when there are no reviews; handled reaching the last page before reaching PAGES page

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'https://store.steampowered.com/search/?start={0}&count=50&maxprice={1}&category1=998&untags={2}&unvrsupport=401&os=win&supportedlang=english&ignore_preferences=1'
+BASE_URL = 'https://store.steampowered.com/search/?start={0}&count=50&maxprice={1}&category1=998&untags=3799%2C4085%2C9130%2C9551&unvrsupport=401&os=win&supportedlang=english'
 PAGES = 1
 
-MIN_RATING = 80
+MIN_REVIEW_PERCENT = 80
+MIN_REVIEW_COUNT = 100
 MAX_PRICE = 30
-BLACKLIST = ['3799', '4085', '9130', '9551']
 
 games = []
 
 for page in range(max(PAGES, 1)):
     print(f'Fetching page {page + 1}/{max(PAGES, 1)}')
 
-    url = BASE_URL.format(page * 50, MAX_PRICE, "%2C".join(BLACKLIST))
+    url = BASE_URL.format(page * 50, MAX_PRICE)
 
     # send request and validate response
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=30)
 
     if response.status_code != 200:
         print(f'Error fetching page {page + 1} ({response.status_code})')
-
         continue
 
     # parse data from response
 
     content = BeautifulSoup(response.content, 'html.parser')
-
-    # TODO: maybe use this?
-    # total_games = int(content.find(class_='search_pagination_left').text.strip().split(' ')[-1])
-
     rows = content.find_all(class_='search_result_row')
 
     for row in rows:
@@ -39,13 +34,20 @@ for page in range(max(PAGES, 1)):
 
         # get reviews
 
-        elem = row.find(class_='search_review_summary').get('data-tooltip-html').split('<br>')[1]
-        review_percent = int(elem.split(' ')[0][:-1])
-        review_count = int(elem.split(' ')[3].replace(',', ''))
+        elem = row.find(class_='search_review_summary')
+
+        if elem:
+            review_text = elem.get('data-tooltip-html').split('<br>')[1]
+            review_percent = int(review_text.split(' ')[0][:-1])
+            review_count = int(review_text.split(' ')[3].replace(',', ''))
+        else:
+            review_percent = 0
+            review_count = 0
 
         # get price
 
         elem = row.find(class_='discount_final_price')
+
         if elem and elem.text != 'Free':
             price = float(elem.text[:-1].replace(',', '.'))
         else:
@@ -54,6 +56,7 @@ for page in range(max(PAGES, 1)):
         # get discount
 
         elem = row.find(class_='discount_pct')
+
         if elem:
             discount = int(elem.text[1:-1])
         else:
@@ -61,11 +64,18 @@ for page in range(max(PAGES, 1)):
 
         # filter and add game to list
 
-        if review_percent >= MIN_RATING:
+        if review_percent >= MIN_REVIEW_PERCENT and review_count >= MIN_REVIEW_COUNT:
             games.append([link, name, review_percent, review_count, price, discount])
+    
+    # if somehow the last page is reached, exit the loop early
+
+    if len(rows) < 50:
+        break
 
 # TODO: sort results
 # TODO: output into a file
 
 for game in games:
     print(game[1:])
+
+print(f'{len(games)} games found.')
